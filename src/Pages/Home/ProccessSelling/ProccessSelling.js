70bec8e1f3c955c6ef13583b06743cd4bced6472
arifhossain <manabethreeplusone@gmail.com>
2022-11-24 15:55:27 +0600

    second commit

---
+++ src/Pages/Home/ProccessSelling/ProccessSelling.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProccessSelling = () => {
+    return (
+        <div className="hero min-h-screen bg-white-900">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">HOW SELLING USED CAMERA EQUIPMENT TO KEH WORKS</h1>
+                    <p className="py-6"><span className='text-3xl font-bold'>1.</span>
+                        Get an instant, top-dollar quote on your gear by using our online estimator. Or, for a more accurate quote, make an appointment for a video chat with one of our Expert Buyers, where they’ll be able to better assess the condition of your used camera equipment.
+                        <span className='text-3xl font-bold'>2.</span>
+                        Once you have your quote, simply print out your free shipping label and pack up your gear to ship. For quotes over $300, let us arrange a convenient front-door pickup with FedEx for your box—simply call 1-800-DIALKEH to make arrangements.
+                        <span className='text-3xl font-bold'>3.</span>
+                        When we receive your gear, our Gear Experts will inspect it and determine the final offer. Accept the offer to get paid via check or Paypal, trade for another item, or get store credit. If you deny our offer, we’ll ship your gear back free of charge.</p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProccessSelling;
