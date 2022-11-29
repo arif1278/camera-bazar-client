34e02d5a1d6bfda9c6a7f1c8115e187d53816905
arifhossain <manabethreeplusone@gmail.com>
2022-11-29 20:53:13 +0600

    twenty commit

---
+++ src/Pages/Home/AdvertisedProducts/AdvertisedProducts.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import BookingModal from '../../../Components/BookingModal/BookingModal';
+import AdvertisedProductCard from './AdvertisedProductCard';
+
+const AdvertisedProducts = ({ advertisedProducts, refetch }) => {
+
+    const [bookProduct, setBookProduct] = useState(null);
+
+    return (
+        <div className='container mx-auto max-w-screen-xl px-2 md:px-4 xl:px-0'>
+            <h2 className='text-2xl font-bold'>Advertised Products</h2>
+            <div className='divider bg-neutral-content h-0.5 opacity-50 mt-2 mb-6'></div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+                {
+                    advertisedProducts.map(product => <AdvertisedProductCard key={product._id} product={product} setBookProduct={setBookProduct}></AdvertisedProductCard>)
+                }
+            </div>
+            {
+                bookProduct &&
+                <BookingModal bookProduct={bookProduct} setBookProduct={setBookProduct} refetch={refetch}></BookingModal>
+            }
+        </div>
+    );
+};
+
+export default AdvertisedProducts;
